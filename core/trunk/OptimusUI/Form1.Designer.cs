2f248b6998d847172602a112c042d0a37cab808f
harald.roexeisen <harald.roexeisen@7ebfdd4c-832b-0410-bac6-d5f825833f54>
2007-03-16 03:42:12 +0000

    Set version to 0.2.0.1.

--- core/trunk/OptimusUI/Form1.Designer.cs
+++ core/trunk/OptimusUI/Form1.Designer.cs
@@ -518,7 +518,7 @@ namespace OptimusUI
       this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
       this.MaximizeBox = false;
       this.Name = "Form1";
-      this.Text = "Optimus mini library tester v0.2.0.0";
+      this.Text = "Optimus mini library tester v0.2.0.1";
       this.Resize += new System.EventHandler(this.Form1_Resize);
       this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
       this.groupSettings.ResumeLayout(false);
